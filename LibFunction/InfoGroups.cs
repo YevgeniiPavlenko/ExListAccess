b655c5701761c6d952d4e7995799c6fa7fc12d3d
Yevgenii.Pavlenko <Yevgenii.Pavlenko@gmail.com>
2020-02-10 22:49:23 +0200

    Released February 20, 2020. Created 10:42 p.m.

---
+++ LibFunction/InfoGroups.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibFunction
+{
+    class XGroup
+    {
+        string x__department = "";
+        string x__title = "";
+        string x__company = "";
+
+        /// <summary>
+        /// Description All Constructor in the "XGroup" Class
+        /// </summary>
+        #region XGroup__Constructors
+        public XGroup()
+        {
+            x__company = "";
+            x__department = "";
+            x__title = "";
+        }
+
+        public XGroup(string xcompany, string xdepartment, string xtitle)
+        {
+            x__company = xcompany;
+            x__department = xdepartment;
+            x__title = xtitle;
+        }
+        #endregion
+
+        /// <summary>
+        /// Description All Property in the "XGroup" Class
+        /// </summary>
+        #region Properties
+        /// <summary>
+        /// Set Or Get Properties "x__departmet"
+        /// </summary>
+        public string  p_department {
+            get 
+            {
+                return x__department;
+            }
+            set 
+            {
+                x__department = value;
+            }
+        }
+        
+        /// <summary>
+        /// Set Or Get Properties "x__title"
+        /// </summary>
+        public string p_title
+        {
+            get
+            {
+                return x__title;
+            }
+            set
+            {
+                x__title = value;
+            }
+        }
+
+        /// <summary>
+        /// Set Or Get Properties "x__company"
+        /// </summary>
+        public string p_company
+        {
+            get
+            {
+                return x__company;
+            }
+            set
+            {
+                x__company = value;
+            }
+        }
+        #endregion
+
+    }
+
+    class InfoXGroups
+    {
+        const string x__NameXMLConfigConst = "InfXGroups.xml";
+
+        List <XGroup> x__groupList = new List<XGroup> ();
+        string x__PachXMLConfig;
+        string x__NameXMLConfig;
+
+        /// <summary>
+        /// Description All Constructor in the "InfoXGroups" Class
+        /// </summary>
+        #region InfoXGroups_Constructors
+        public InfoXGroups()
+        {
+            x__PachXMLConfig = Environment.CurrentDirectory;
+            x__NameXMLConfig = x__NameXMLConfigConst;
+        }
+
+        public InfoXGroups(string p_CurrentDirectory)
+        {
+            if ((p_CurrentDirectory == null) || (p_CurrentDirectory == ""))
+            {
+                x__PachXMLConfig = Environment.CurrentDirectory;
+            } 
+            else
+            {
+                if (System.IO.Directory.Exists(p_CurrentDirectory))
+                {
+                    x__PachXMLConfig = p_CurrentDirectory;
+                }
+                else
+                {
+                    try
+                    {
+                        System.IO.Directory.CreateDirectory(p_CurrentDirectory);
+                        x__PachXMLConfig = p_CurrentDirectory;
+                    }
+                    catch
+                    {
+                        x__PachXMLConfig = Environment.CurrentDirectory;
+                    }
+                }
+            }
+
+            x__NameXMLConfig = x__NameXMLConfigConst;
+        }
+
+        public InfoXGroups (string p_CurrentDirectory = "", string p_NameXMLConfig = "")
+        {
+            if ((p_CurrentDirectory == null) || (p_CurrentDirectory == ""))
+            {
+                x__PachXMLConfig = Environment.CurrentDirectory;
+            }
+            else
+            {
+                if (System.IO.Directory.Exists(p_CurrentDirectory))
+                {
+                    x__PachXMLConfig = p_CurrentDirectory;
+                }
+                else
+                {
+                    try
+                    {
+                        System.IO.Directory.CreateDirectory(p_CurrentDirectory);
+                        if (System.IO.Directory.Exists(p_CurrentDirectory)) { x__PachXMLConfig = p_CurrentDirectory; }
+                    }
+                    catch
+                    {
+                        x__PachXMLConfig = Environment.CurrentDirectory;
+                    }
+                }
+            }
+
+            if ((p_NameXMLConfig == "") || (p_NameXMLConfig == null))
+            {
+                x__NameXMLConfig = x__NameXMLConfigConst;
+            }
+            else
+            {
+                if (System.IO.File.Exists(System.IO.Path.Combine(x__PachXMLConfig, p_NameXMLConfig)))
+                {
+                    x__NameXMLConfig = p_NameXMLConfig;
+                }
+                else
+                {
+                    try
+                    {
+                        System.IO.File.CreateText(System.IO.Path.Combine(x__PachXMLConfig, p_NameXMLConfig));
+                        
+                    }
+                    catch
+                    {
+                        x__NameXMLConfig = x__NameXMLConfigConst;
+                    }
+                }
+            }
+            
+        }
+        #endregion
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,14 +1,6 @@
 "# ExListAccess" 
 =====================================================
-Released February 02, 2020
 
-Made by:
-Added a separate library for functions that require .Net Freymvork 4.7.2. Functions for receiving parameters with AD are written. Written a separate class for storing user information.
-
-Planning:
-To develop a mechanism for determining information about groups of a domain controller.
-
-=====================================================
 Released February 03, 2020
 
 Fixed a bug in the constructor of the GetAtrUsersReads class.
@@ -34,4 +26,14 @@ It is planned:
 - Further in the future, develop based on the above paragraph, a mechanism that will make changes to the domain controller.
 - Further complete the preservation of information about the work done by the program.
 - make an application, a separate window in which it will be possible to watch, edit the information of the configuration file according to the criteria for providing information.
+=====================================================
+
+Released February 02, 2020
+
+Made by:
+Added a separate library for functions that require .Net Freymvork 4.7.2. Functions for receiving parameters with AD are written. Written a separate class for storing user information.
+
+Planning:
+To develop a mechanism for determining information about groups of a domain controller.
+
 =====================================================
